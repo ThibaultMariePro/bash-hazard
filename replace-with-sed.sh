7eecd2f5a452795b39018f68da808168d346de8a
TIBO <thibault.marie.pro@gmail.com>
2024-03-22 23:12:34 +0100

    fixed color var

--- replace-with-sed.sh
+++ replace-with-sed.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo replaced $1 by $2 in $3, $3.ori saved
+BOLDBLUE="\e[1;${GREEN}"
+ENDCOLOR="\e[0m"
+
+echo -e "${BOLDBLUE} replaced $1 by $2 in $3, $3.ori saved."
 
 sed -i.ori 's/'$1'/'$2'/g' $3
